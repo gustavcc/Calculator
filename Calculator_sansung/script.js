c061771ac3fe66e02baa7deac5858c5421f1d331
Gustavo Cardoso Costa <ritacardoso290@gmail.com>
2023-12-12 22:47:20 -0300

    modify

--- Calculator_sansung/script.js
+++ Calculator_sansung/script.js
@@ -15,14 +15,35 @@ var multiplicar = document.getElementById('multiplicar');
 var c = document.getElementById('C');
 
 var resultado = document.getElementById('result');
+var resultado_final = document.getElementById('final');
 
 function mostrarResultado(x) {
+
     var numero = document.getElementById('result').innerHTML;
-    resultado.innerHTML = numero + x;
+
+    if (x == '=') {
+        if (String(numero).includes('x')){
+            const mult = resultado.innerHTML.split(' x ');
+            final.innerHTML = parseFloat(mult[0]) * parseFloat(mult[1])
+        } else if (String(numero).includes('+')){
+            const soma = resultado.innerHTML.split(' + ');
+            final.innerHTML = parseFloat(soma[0]) + parseFloat(soma[1])
+        } else if (String(numero).includes('-')){
+            const sub = resultado.innerHTML.split(' - ');
+            final.innerHTML = parseFloat(sub[0]) - parseFloat(sub[1])
+        } else if (String(numero).includes('/')){
+            const div = resultado.innerHTML.split(' / ');
+            final.innerHTML = parseFloat(div[0]) / parseFloat(div[1])
+        }
+    }
+    else {
+        resultado.innerHTML = numero + x;
+    }
 }
 
 function limparResultado() {
     c.addEventListener('click', () => {
         resultado.innerHTML = '';
+        final.innerHTML = '';
     })
 }
